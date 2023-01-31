d055570dc3634d974cee5557ea3c34e076f2a22c
Łukasz Justyński <lukasz.justynski@gmail.com>
2023-01-31 15:24:10 +0100

    Add search_data function + small code fixes

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ window.config(padx=20, pady=20, bg="#F5EFE6")
 
 
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -35,7 +37,8 @@ def save_data():
     if len(enter_website.get()) == 0 or len(enter_user.get()) == 0 or len(new_password.get()) == 0:
         messagebox.showwarning(title="Error", message="Please fill all the empty fields.")
     else:
-        yes = messagebox.askyesno(title="Saving", message=f"Are you sure you want to save these details? "
+        yes = messagebox.askyesno(title="Saving",
+                                  message=f"Are you sure you want to save these details? "
                                           f"\n{enter_website.get()}\n{enter_user.get()}\n{new_password.get()}")
         if yes:
             try:
@@ -54,26 +57,47 @@ def save_data():
                 new_password.delete(0, END)
 
 
+def search_data():
+    website = enter_website.get()
+    try:
+        with open("data.json", "r") as data:
+            data_file = json.load(data)
+    except FileNotFoundError:
+        messagebox.showinfo(title="No data found", message="Sorry, no record were found. Please check website name "
+                                                           "and try again.")
+    else:
+        if website in data_file:
+            email = data_file[website]["email"]
+            password = data_file[website]["password"]
+            messagebox.showinfo(title="Results", message=f"Website: {website}\nEmail: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="No data found", message="Sorry, no record were found. Please check website name "
+                                                               "and try again.")
+
+
 canvas = Canvas(width=260, height=260, bg="#F5EFE6", highlightthickness=0)
 padlock_img = PhotoImage(file="padlock.png")
 canvas.create_image(130, 130, image=padlock_img)
 canvas.grid(column=1, row=0, pady=20)
 
-website = Label(text="Website:", font=("Verdana", 10), bg="#F5EFE6")
-website.grid(column=0, row=1, pady=2)
+website_label = Label(text="Website:", font=("Verdana", 10), bg="#F5EFE6")
+website_label.grid(column=0, row=1, pady=2)
 
-enter_website = Entry(width=60)
+enter_website = Entry(width=42)
 enter_website.focus()
-enter_website.grid(column=1, row=1, columnspan=2, pady=2)
+enter_website.grid(column=1, row=1, pady=2)
 
-username = Label(text="Email / Username:", font=("Verdana", 10), bg="#F5EFE6")
-username.grid(column=0, row=2)
+search_btn = Button(text="Search", width=14, command=search_data)
+search_btn.grid(column=2, row=1)
+
+username_label = Label(text="Email / Username:", font=("Verdana", 10), bg="#F5EFE6")
+username_label.grid(column=0, row=2)
 
 enter_user = Entry(width=60)
 enter_user.grid(column=1, row=2, columnspan=2)
 
-password = Label(text="Password:", font=("Verdana", 10), bg="#F5EFE6")
-password.grid(column=0, row=3)
+password_label = Label(text="Password:", font=("Verdana", 10), bg="#F5EFE6")
+password_label.grid(column=0, row=3)
 
 new_password = Entry(width=42)
 new_password.grid(column=1, row=3)
@@ -83,6 +107,5 @@ generate_btn.grid(column=2, row=3)
 
 add_btn = Button(text="Add", width=50, command=save_data)
 add_btn.grid(column=1, row=4, columnspan=2, pady=5)
-
 
 window.mainloop()
